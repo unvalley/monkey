3f52d4a57a436f6c865f4d5d65dc5110f4205200
unvalley <kirohi.code@gmail.com>
2022-09-01 03:05:05 +0900

    feat: update test case and pass it

--- lib/lexer/mod.rs
+++ lib/lexer/mod.rs
@@ -34,7 +34,40 @@ impl Lexer {
         self.read_position += 1;
     }
 
+    fn is_letter(&mut self, ch: u8) -> bool {
+        (b'a'..=b'z').contains(&ch) || (b'A'..=b'Z').contains(&ch) || ch == b'_'
+    }
+
+    fn is_digit(&mut self, ch: u8) -> bool {
+        (b'0'..=b'9').contains(&ch)
+    }
+
+    fn read_int(&mut self, ch: u8) -> i64 {
+        let position = self.position;
+        while self.is_digit(ch) {
+            self.read_char()
+        }
+        let int = &self.input[position..self.position];
+        int.parse::<i64>().unwrap()
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let position = self.position;
+        while self.is_letter(self.ch) {
+            self.read_char()
+        }
+        self.input[position..self.position].to_string()
+    }
+
+    fn skip_whitespace(&mut self) {
+        while let b' ' | b'\t' | b'\n' | b'\r' = self.ch {
+            self.read_char()
+        }
+    }
+
     pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+
         let tok = match self.ch {
             b'=' => Token::Assign,
             b'+' => Token::Plus,
@@ -42,7 +75,21 @@ impl Lexer {
             b')' => Token::RParen,
             b'{' => Token::LBrace,
             b'}' => Token::RBrace,
-            _ => Token::Illegal,
+            _ => {
+                if self.is_letter(self.ch) {
+                    let literal = self.read_identifier();
+                    match literal.as_str() {
+                        "fn" => Token::Function,
+                        "let" => Token::Let,
+                        _ => Token::Ident(literal)
+                    }
+                } else if self.is_digit(self.ch) {
+                    let int = self.read_int(self.ch);
+                    Token::IntLiteral(int)
+                } else {
+                    Token::Illegal
+                }
+            },
         };
         self.read_char();
         tok
@@ -55,8 +102,51 @@ mod tests {
 
     #[test]
     fn next_token() {
-        let input = "=+(){},;";
-        let expected_tokens = vec![Token::Assign, Token::Plus, Token::LParen, Token::RParen];
+        let input = "let five = 5;
+        let ten = 10;
+
+        let add = fn(x, y) {
+            x + y;
+        };
+        let result = add(five, ten);
+        ";
+        let expected_tokens = vec![
+            Token::Let,
+            Token::Ident(String::from("five")),
+            Token::Assign,
+            Token::IntLiteral(5),
+            Token::SemiColon,
+            Token::Let,
+            Token::Ident(String::from("ten")),
+            Token::Assign,
+            Token::IntLiteral(10),
+            Token::SemiColon,
+            Token::Let,
+            Token::Ident(String::from("add")),
+            Token::Assign,
+            Token::Function,
+            Token::LParen,
+            Token::Ident(String::from("x")),
+            Token::Comma,
+            Token::Ident(String::from("y")),
+            Token::RParen,
+            Token::LBrace,
+            Token::Ident(String::from("x")),
+            Token::Plus,
+            Token::Ident(String::from("y")),
+            Token::SemiColon,
+            Token::RBrace,
+            Token::SemiColon,
+            Token::Let,
+            Token::Ident(String::from("result")),
+            Token::Assign,
+            Token::Ident(String::from("add")),
+            Token::LBrace,
+            Token::Ident(String::from("five")),
+            Token::Comma,
+            Token::Ident(String::from("ten")),
+            Token::SemiColon,
+        ];
 
         let mut l = Lexer::new(input.to_string());
 
